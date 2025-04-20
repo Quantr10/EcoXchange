9f5753a424961f9bd88ca2ca08f711b1cb7d4c5b
Quan Tran <thequantran2005@gmail.com>
2025-04-19 21:02:33 -0400

    finished auth

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { ToastContainer, toast } from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { Home, Contact, Cart, Admin, OrderHistory, Login, Register, Reset} from "./pages";
 import { Header, Footer } from "./components";
