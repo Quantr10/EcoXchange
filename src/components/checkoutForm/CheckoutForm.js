f56d03612f5c708d973508c67c230ac84db1dfb3
Quan Tran <thequantran2005@gmail.com>
2025-04-30 02:51:37 -0400

    Fix file tracking and case-sensitive rename

---
+++ src/components/checkoutForm/CheckoutForm.js
@@ -0,0 +1,85 @@
+import React, { useState } from "react";
+import {
+  PaymentElement,
+  useStripe,
+  useElements,
+} from "@stripe/react-stripe-js";
+import styles from "./CheckoutForm.module.css";
+import CheckoutSummary from "../checkoutSummary/CheckoutSummary";
+import spinnerImg from "../../assets/spinner.jpg";
+import Card from "../card/Card";
+
+const CheckoutForm = () => {
+  const stripe = useStripe();
+  const elements = useElements();
+
+  const [message, setMessage] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!stripe || !elements) {
+      // Stripe.js hasn't yet loaded.
+      // Make sure to disable form submission until Stripe.js has loaded.
+      return;
+    }
+
+    setIsLoading(true);
+
+    const { error } = await stripe.confirmPayment({
+      elements,
+      confirmParams: {
+        // Make sure to change this to your payment completion page
+        return_url: "http://localhost:3000/complete",
+      },
+    });
+
+    setIsLoading(false);
+  };
+
+  const paymentElementOptions = {
+    layout: "accordion",
+  };
+
+  return (
+    <section>
+      <div className={`containter ${styles.checkout}`}>
+        <h2>Checkout</h2>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <Card cardClass={styles.card}>
+              <CheckoutSummary />
+            </Card>
+          </div>
+          <div>
+            <Card cardClass={`${styles.card} ${styles.pay}`}>
+              <h3>Stripe Checkout</h3>
+              <PaymentElement id={styles["payment-element"]} />
+              <button
+                disabled={isLoading || !stripe || !elements}
+                id="submit"
+                className={styles.button}
+              >
+                <span id="button-text">
+                  {isLoading ? (
+                    <img
+                      src={spinnerImg}
+                      alt="Loading..."
+                      style={{ width: "20px" }}
+                    />
+                  ) : (
+                    "Pay now"
+                  )}
+                </span>
+              </button>
+              {message && <div id={styles["payment-message"]}>{message}</div>}
+            </Card>
+          </div>
+        </form>
+      </div>
+    </section>
+  );
+};
+
+export default CheckoutForm;
