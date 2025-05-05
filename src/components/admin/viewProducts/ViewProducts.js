d689288d32aeac73a5f0671ed72a023e2d60d5cb
Quan Tran <thequantran2005@gmail.com>
2025-05-05 02:15:47 -0400

    add-chart

--- src/components/admin/viewProducts/ViewProducts.js
+++ src/components/admin/viewProducts/ViewProducts.js
@@ -1,20 +1,33 @@
-import React, { useEffect, useState } from 'react';
-import styles from "./ViewProducts.module.css"
-import { toast } from 'react-toastify';
-import { db, storage } from '../../../firebase/config';
-import { collection, deleteDoc, doc, onSnapshot, orderBy, query } from 'firebase/firestore';
-import { FaEdit, FaTrashAlt } from 'react-icons/fa';
-import { Link } from 'react-router-dom';
-import Loader from '../../loader/Loader';
-import { deleteObject, ref } from 'firebase/storage';
-import Notiflix from 'notiflix';
-import { useDispatch, useSelector } from 'react-redux';
-import { selectProducts, STORE_PRODUCTS } from '../../../redux/slice/productSlice';
-import useFetchCollection from '../../../customHooks/useFetchCollection';
+import React, { useEffect, useState } from "react";
+import styles from "./ViewProducts.module.css";
+import { toast } from "react-toastify";
+import { db, storage } from "../../../firebase/config";
+import {
+  deleteDoc,
+  doc,
+} from "firebase/firestore";
+import { FaEdit, FaTrashAlt } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import Loader from "../../loader/Loader";
+import { deleteObject, ref } from "firebase/storage";
+import Notiflix from "notiflix";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  selectProducts,
+  STORE_PRODUCTS,
+} from "../../../redux/slice/productSlice";
+import useFetchCollection from "../../../customHooks/useFetchCollection";
+import {
+  FILTER_BY_SEARCH,
+  selectFilteredProducts,
+} from "../../../redux/slice/filterSlice";
+import Search from "../../search/Search";
 
 const ViewProducts = () => {
+  const [search, setSearch] = useState("");
   const { data, isLoading } = useFetchCollection("products");
   const products = useSelector(selectProducts);
+  const filteredProducts = useSelector(selectFilteredProducts);
 
   const dispatch = useDispatch();
 
@@ -23,49 +36,60 @@ const ViewProducts = () => {
       STORE_PRODUCTS({
         products: data,
       })
-    )
-  }, [dispatch, data])
+    );
+  }, [dispatch, data]);
+
+  useEffect(() => {
+    dispatch(FILTER_BY_SEARCH({products, search}));
+  }, [dispatch, products, search])
 
   const confirmDelete = (id, imageURL) => {
     Notiflix.Confirm.show(
-      'Delete Product!!!',
-      'You are about the delete this product',
-      'Delete',
-      'Cancel',
+      "Delete Product!!!",
+      "You are about the delete this product",
+      "Delete",
+      "Cancel",
       function okCb() {
-        deleteProduct(id, imageURL)
+        deleteProduct(id, imageURL);
       },
       function cancelCb() {
-        console.log("Delete canceled")
+        console.log("Delete canceled");
       },
       {
-        width: '320px',
-        borderRadius: '3px',
+        width: "320px",
+        borderRadius: "3px",
         titleColor: "orangered",
         okButtonBackground: "orangered",
         cssAnimationStyle: "zoom",
-      },
+      }
     );
-  }
+  };
 
   const deleteProduct = async (id, imageURL) => {
     try {
       await deleteDoc(doc(db, "products", id));
       const storageRef = ref(storage, imageURL);
-      await deleteObject(storageRef)
-      toast.success("Product deleted successfully")
-    } catch(error) {
-      toast.error(error.message)
+      await deleteObject(storageRef);
+      toast.success("Product deleted successfully");
+    } catch (error) {
+      toast.error(error.message);
     }
-  }
+  };
 
   return (
     <>
-      {isLoading && <Loader/>}
+      {isLoading && <Loader />}
       <div className={styles.table}>
         <h2>All Products</h2>
 
-        {products.length ===0 ? (
+        <div className={styles.search}>
+          <p>
+            <b>{filteredProducts.length}</b> products found
+          </p>
+          <Search value={search} onChange={(e) => setSearch(e.target.value)} />
+        </div>
+
+        {filteredProducts.length === 0 ? (
           <p>No product found.</p>
         ) : (
           <table>
@@ -76,36 +100,35 @@ const ViewProducts = () => {
                 <th>Name</th>
                 <th>Category</th>
                 <th>Price</th>
-                <th>Actions</th>  
+                <th>Actions</th>
               </tr>
             </thead>
             <tbody>
-              {products.map((product, index) => {
-                const {id, name, price, imageURL, category} = product;
+              {filteredProducts.map((product, index) => {
+                const { id, name, price, imageURL, category } = product;
                 return (
                   <tr key={id}>
+                    <td>{index + 1}</td>
                     <td>
-                      {index + 1}
+                      <img
+                        src={imageURL}
+                        alt={name}
+                        style={{ width: "100px" }}
+                      />
                     </td>
-                    <td>
-                      <img src={imageURL} alt={name} style={{width: "100px"}} />
-                    </td>
-                    <td>
-                      {name}
-                    </td>
+                    <td>{name}</td>
                     <td>{category}</td>
-                    <td>
-                      {`$${price}`}
-                    </td>
+                    <td>{`$${price}`}</td>
                     <td className={styles.icons}>
                       <Link to={`/admin/add-product/${id}`}>
-                        <FaEdit size={20} color="green"/>
+                        <FaEdit size={20} color="green" />
                       </Link>
                       &nbsp;
-                      <FaTrashAlt 
+                      <FaTrashAlt
                         size={18}
                         color="red"
-                        onClick={() => confirmDelete(id, imageURL)}/>
+                        onClick={() => confirmDelete(id, imageURL)}
+                      />
                     </td>
                   </tr>
                 );
@@ -115,8 +138,7 @@ const ViewProducts = () => {
         )}
       </div>
     </>
-    
-  )
-}
+  );
+};
 
-export default ViewProducts
+export default ViewProducts;
